961eb1c77fae13ff483a922aa1265c8aac85a780
Oguzkaan93 <102527193+Oguzkaan93@users.noreply.github.com>
2023-01-20 11:29:21 +0300

    Update Find_min_max_Postgre_internal.sql

--- Find_min_max_Postgre_internal.sql
+++ Find_min_max_Postgre_internal.sql
@@ -1,23 +1,4 @@
-CREATE or replace FUNCTION find_min_and_max_func(
-    state point,
-    next  integer
-) RETURNS point
-LANGUAGE plpgsql
-STRICT
-AS $$
-declare
-min_val integer;  
-max_val integer;                   
-begin    
-if state[0] <= next then min_val := state[0];  
-elsif next < state[0] then min_val := next;  
-end if;
-if state[1] >= next then max_val := state[1];  
-elsif next > state[1] then max_val := next;  
-end if;               
-return point(min_val, max_val) ;                     
-END;
-$$;
+CREATE or replace FUNCTION find_min_and_max_func(point,integer) RETURNS point AS E'PATH_TO_FILE', 'minmax' LANGUAGE C STRICT ;
 
 CREATE or replace FUNCTION find_min_and_max_final_func(
     state point
@@ -36,7 +17,7 @@ CREATE or replace AGGREGATE find_min_and_max(integer)
     SFUNC    = find_min_and_max_func, -- State function
     STYPE    = point,       -- State type
     FINALFUNC = find_min_and_max_final_func,
-    initcond = '(1231231232131,0)'
+    initcond = '(0,0)'
 );
 
 ## Sample Query ##
