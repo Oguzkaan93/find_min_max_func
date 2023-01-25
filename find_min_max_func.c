f3d6d68a561e61fc67f6a5adb1a5e8020db9e9bb
Oguzkaan93 <102527193+Oguzkaan93@users.noreply.github.com>
2023-01-25 14:09:24 +0300

    Create find_min_max_func.c

---
+++ find_min_max_func.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include "postgres.h"
+#include "fmgr.h"
+#include "utils/geo_decls.h"
+PG_MODULE_MAGIC;
+
+
+PG_FUNCTION_INFO_V1(minmax);
+
+Datum
+minmax (PG_FUNCTION_ARGS)
+{
+
+        Point *funk =  PG_GETARG_POINT_P(0);
+        int64 deger = PG_GETARG_INT64(1);
+        int64 funkx = funk->x;
+        int64 funky = funk->y;
+        if ( funkx ==  0 && funky == 0)
+        {
+                funkx = deger;
+                funky = deger;
+        }
+
+        else if ( deger < funkx )
+        {
+                funkx = deger;
+        }
+        else if ( deger > funky )
+        {
+                funky = deger;
+        }
+
+        funk->x = funkx;
+        funk->y = funky;
+        PG_RETURN_POINT_P(funk);
+
+}
